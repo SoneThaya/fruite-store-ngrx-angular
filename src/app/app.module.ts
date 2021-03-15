77f52300d1420a583376faab4974d060aed0521b
Sone <sthaya@gmail.com>
2021-03-14 20:37:10 -0700

    created effects file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { ShopEffects } from './store/effects';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
@@ -8,6 +9,7 @@ import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 
 import { ShopReducer } from './store/reducer';
+import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, HeaderComponent],
@@ -15,6 +17,7 @@ import { ShopReducer } from './store/reducer';
     BrowserModule,
     HttpClientModule,
     StoreModule.forRoot({ shop: ShopReducer }),
+    EffectsModule.forRoot([ShopEffects])
   ],
   providers: [],
   bootstrap: [AppComponent],
