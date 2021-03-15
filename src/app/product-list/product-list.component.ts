7dc93f999693352662f68dedaa86b12afe23f4e4
Sone <sthaya@gmail.com>
2021-03-14 20:40:35 -0700

    created product list component

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Product } from '../product/product.component';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.scss']
+  styleUrls: ['./product-list.component.scss'],
 })
 export class ProductListComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  @Input() fruits: Product[] = [];
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit() {}
 }
